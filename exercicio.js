9f7ba5ff6b8a6499036c2e2a107286367917367e
angelo pedrosa <angelopedrosa00@gmail.com>
2023-05-15 13:04:51 -0300

    exercicio finalizado

---
+++ exercicio.js
@@ -0,0 +1,7 @@
+"use strict";
+const multiplicarNumero = (numero1, numero2) => numero1 * numero2;
+console.log(multiplicarNumero(2, 5));
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(dizOla("Angelo"));
---
+++ exercicio.ts
@@ -0,0 +1,9 @@
+const multiplicarNumero = (numero1: number, numero2: number) : number => numero1 * numero2
+
+console.log(multiplicarNumero(2, 5))
+
+function dizOla(nome: string): string{
+    return `Olá ${nome}`
+}
+
+console.log(dizOla("Angelo"))
